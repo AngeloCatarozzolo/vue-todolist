8c12d29149abd6647597a10c559ff0df68766b95
AngeloCatarozzolo <angelocatarozzolo@gmail.com>
2022-11-16 02:51:25 +0100

    to do list

---
+++ js/main.js
@@ -0,0 +1,39 @@
+new Vue({
+    el:"#app",
+    data:{
+        tasks:[
+            {
+                text:'Fare i compiti',
+                done:true
+            },
+            {
+                text:'Fare la spesa',
+                done:false
+            },
+            {
+                text:'Andare dalla nonna',
+                done:true
+            }
+        ],
+        temporaryTask:{
+            text:'',
+            done:false
+        },
+    },
+    methods:{
+        removeTask:function(index){
+            this.tasks.splice(index,1);
+        },
+        addTask:function(){
+            this.tasks.push(this.temporaryTask);
+            this.temporaryTask={
+                text:'',
+                done:false
+            }
+        },
+        check:function(index){
+           this.tasks[index].done=!this.tasks[index].done
+        }
+    }
+
+})
